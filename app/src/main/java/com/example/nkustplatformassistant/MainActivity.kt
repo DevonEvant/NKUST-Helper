b547740a1e5b9273f3eb96771b2e0ba873194122
DevonEvant <horningch@gmail.com>
2022-07-26 17:41:06 +0800

    *

--- app/src/main/java/com/example/nkustplatformassistant/MainActivity.kt
+++ app/src/main/java/com/example/nkustplatformassistant/MainActivity.kt
@@ -1,17 +1,28 @@
 package com.example.nkustplatformassistant
 
+import android.R.color
+import android.graphics.Bitmap
+import android.graphics.BitmapFactory
 import android.os.Bundle
+import android.util.Log
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.Image
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.ImageBitmap
+import androidx.compose.ui.graphics.asImageBitmap
 import androidx.compose.ui.tooling.preview.Preview
 import com.example.nkustplatformassistant.ui.theme.Nkust_platform_assistantTheme
+import java.util.*
 
+/**
+ * Interface to the Login data layer.
+ */
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -23,10 +34,16 @@ class MainActivity : ComponentActivity() {
                     color = MaterialTheme.colorScheme.background
                 ) {
                     Greeting("Android")
+//                    Image(bitmap = addition_isCorrect(), contentDescription = "123")
+
                 }
             }
         }
     }
+
+
+
+
 }
 
 @Composable
