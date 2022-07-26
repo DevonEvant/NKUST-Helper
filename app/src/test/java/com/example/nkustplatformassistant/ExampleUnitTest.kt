4f5173988a88c13661ced7d42fa7385a303cc370
DevonEvant <horningch@gmail.com>
2022-07-26 17:39:18 +0800

    *

--- app/src/test/java/com/example/nkustplatformassistant/ExampleUnitTest.kt
+++ app/src/test/java/com/example/nkustplatformassistant/ExampleUnitTest.kt
@@ -1,10 +1,17 @@
 package com.example.nkustplatformassistant
 
+
 import android.graphics.BitmapFactory
+import android.util.Log
+import com.example.nkustplatformassistant.data.NKUST_ROUTES
 import org.junit.Test
-import android.content.res.Resources
+import com.example.nkustplatformassistant.data.remote.login
+//import android.content.res.Resources
+
 
 import org.junit.Assert.*
+import kotlinx.coroutines.*
+
 
 /**
  * Example local unit test, which will execute on the development machine (host).
@@ -12,10 +19,19 @@ import org.junit.Assert.*
  * See [testing documentation](http://d.android.com/tools/testing).
  */
 class ExampleUnitTest {
+
     @Test
     fun addition_isCorrect() {
-        val bitmap = BitmapFactory.decodeResource(Resources, R.drawable.validate_code)
+        runBlocking {
+        }
 
-        print(bitmap)
     }
+
+    @Test
+    fun a() {
+        println(NKUST_ROUTES.WEDAP_ETXT_WITH_SYMBOL)
+    }
+
+//    https://webap.nkust.edu.tw/nkust/validateCode.jsp/?it=1.3759458507944256
+//    https://webap.nkust.edu.tw/nkust/validateCode.jsp?it=0.26295017141077903
 }
