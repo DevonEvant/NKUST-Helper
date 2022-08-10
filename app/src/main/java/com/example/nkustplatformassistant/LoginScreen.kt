13dacdef69b2017a69fdd097c615423e4a4728b8
lrnbrad <lrnbrad@gmail.com>
2022-08-10 22:26:06 +0800

    Modify with ViewModel for future usage, dialog logic broken

---
+++ app/src/main/java/com/example/nkustplatformassistant/LoginScreen.kt
@@ -0,0 +1,383 @@
+@file:Suppress("SpellCheckingInspection")
+
+package com.example.nkustplatformassistant
+
+import android.annotation.SuppressLint
+import android.content.Context
+import android.os.Bundle
+import android.widget.Toast
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.gestures.detectTapGestures
+import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.foundation.text.KeyboardActions
+import androidx.compose.foundation.text.KeyboardOptions
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.twotone.*
+import androidx.compose.material3.*
+import androidx.compose.runtime.*
+import androidx.compose.runtime.livedata.observeAsState
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.focus.FocusDirection
+import androidx.compose.ui.focus.focusTarget
+import androidx.compose.ui.graphics.ImageBitmap
+import androidx.compose.ui.input.pointer.pointerInput
+import androidx.compose.ui.platform.LocalContext
+import androidx.compose.ui.platform.LocalFocusManager
+import androidx.compose.ui.text.input.ImeAction
+import androidx.compose.ui.text.input.KeyboardType
+import androidx.compose.ui.text.input.PasswordVisualTransformation
+import androidx.compose.ui.text.input.VisualTransformation
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+import androidx.compose.ui.window.Dialog
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import androidx.lifecycle.viewmodel.compose.viewModel
+import androidx.navigation.NavHostController
+import androidx.navigation.compose.rememberNavController
+import com.example.nkustplatformassistant.data.remote.NkustUser
+import com.example.nkustplatformassistant.ui.theme.Nkust_platform_assistantTheme
+import kotlinx.coroutines.launch
+
+val user = NkustUser()
+
+class LoginScreen : ComponentActivity() {
+    lateinit var navController: NavHostController
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContent() {
+            Nkust_platform_assistantTheme {
+                // navController As start destination
+                navController = rememberNavController()
+//                NkustpaNavGraph(navController = navController)
+
+                // A surface container using the 'background' color from the theme
+                Surface(
+                    modifier = Modifier.fillMaxSize(),
+                    color = MaterialTheme.colorScheme.background
+                ) {
+//                    LoginForm(navController)
+                }
+            }
+        }
+    }
+}
+
+@Preview(showBackground = true)
+@Composable
+fun LoginScreenPreview() {
+    Nkust_platform_assistantTheme {
+        LoginScreenBase()
+    }
+}
+
+// LoginParams State hosting
+class LoginParamsViewModel : ViewModel() {
+    private val _uid: MutableLiveData<String> = MutableLiveData("")
+    private val _pwd: MutableLiveData<String> = MutableLiveData("")
+    private val _pwdVisibility: MutableLiveData<Boolean> = MutableLiveData(false)
+
+    val uid: LiveData<String> = _uid
+    val pwd: LiveData<String> = _pwd
+    val pwdVisibility: LiveData<Boolean> = _pwdVisibility
+
+    fun onUidChange(newUid: String) {
+        _uid.value = newUid
+    }
+
+    fun onPwdChange(newPwd: String) {
+        _pwd.value = newPwd
+    }
+
+    fun onPwdVisibilityReversed() {
+        _pwdVisibility.value = (_pwdVisibility.value)!!.not()
+    }
+}
+
+@Composable
+fun LoginScreenBase(loginParmsViewModel: LoginParamsViewModel = viewModel()) {
+    val uid: String by loginParmsViewModel.uid.observeAsState("")
+    val pwd: String by loginParmsViewModel.pwd.observeAsState("")
+    val pwdVisibility: Boolean by loginParmsViewModel.pwdVisibility.observeAsState(false)
+
+    LoginForm(
+        uid = uid, pwd = pwd, pwdVisibility = pwdVisibility,
+        onUidChanged = { loginParmsViewModel.onUidChange(it) },
+        onPwdChanged = { loginParmsViewModel.onPwdChange(it) },
+        onPwdVisibilityReversed = { loginParmsViewModel.onPwdVisibilityReversed() })
+
+}
+
+
+@Composable
+@OptIn(ExperimentalMaterial3Api::class)
+fun LoginForm(
+//    navController: NavController,
+    uid: String,
+    pwd: String,
+    pwdVisibility: Boolean,
+    onUidChanged: (String) -> Unit,
+    onPwdChanged: (String) -> Unit,
+    onPwdVisibilityReversed: () -> Unit,
+) {
+    val focusManager = LocalFocusManager.current
+    val context = LocalContext.current
+    val etxtCodeViewModel = EtxtCodeViewModel()
+
+    // user.loginWebap(uid, pwd, etxtCode)
+    // navController.navigate(
+    // route = Screen.Home.route,
+    // )
+    // TODO(1. only put etxt to home screen and do login in HomeScreen - bad
+    //      2. create data class to determinate is it okay to logon
+    //         if it's OK, then direct put Home screen
+    //      3. use viewmodel to handling uid, pwd & etxt, then login in HomeScreen - meh)
+
+    Column(
+        modifier = Modifier
+            .padding(start = 10.dp, bottom = 10.dp)
+            // TODO: pointerInput() broken
+            .pointerInput(Unit) {
+                detectTapGestures(onTap = { focusManager.clearFocus() })
+            }
+    ) {
+        Text(text = "Welcome!",
+            modifier = Modifier.padding(bottom = 8.dp))
+        OutlinedTextField(
+            value = uid,
+            onValueChange = onUidChanged,
+            label = { Text(text = "Student ID") },
+            singleLine = true,
+            leadingIcon = {
+                Icon(imageVector = Icons.TwoTone.Person,
+                    "")
+            },
+            shape = RoundedCornerShape(50),
+            modifier = Modifier
+                .focusTarget()
+                .fillMaxWidth(0.85F),
+            keyboardOptions = KeyboardOptions(imeAction = ImeAction.Go),
+            keyboardActions = KeyboardActions(
+                onGo = {
+                    focusManager.moveFocus(FocusDirection.Down)
+                },
+            )
+        )
+        OutlinedTextField(
+            modifier = Modifier.fillMaxWidth(0.85F),
+            value = pwd,
+            onValueChange = onPwdChanged,
+            label = { Text(text = "Password") },
+            singleLine = true,
+            leadingIcon = {
+                Icon(imageVector = Icons.TwoTone.Password,
+                    "")
+            },
+            shape = RoundedCornerShape(50),
+            visualTransformation = if (pwdVisibility) {
+                VisualTransformation.None
+            } else {
+                PasswordVisualTransformation()
+            },
+            keyboardOptions = KeyboardOptions(
+                imeAction = ImeAction.Done,
+                keyboardType = KeyboardType.Password),
+            keyboardActions = KeyboardActions(
+                onDone = {
+                    focusManager.clearFocus()
+                    // TODO: Course Activity
+                }
+            ), trailingIcon = {
+                val image =
+                    if (pwdVisibility) Icons.TwoTone.Visibility
+                    else Icons.TwoTone.VisibilityOff
+                val description =
+                    if (pwdVisibility) "Hide password"
+                    else "Show password"
+                IconButton(onClick = onPwdVisibilityReversed) {
+                    Icon(imageVector = image, description)
+                }
+            }
+        )
+        Spacer(modifier = Modifier.padding(10.dp))
+
+        Row(
+            horizontalArrangement = Arrangement.Center
+        ) {
+            Button(
+                onClick = {
+                    // TODO: Check if the user entered uid and pwd using better way
+                    if (pwd.isEmpty() || uid.isEmpty()) {
+                        Toast.makeText(context,
+                            "ID or PW didn't input",
+                            Toast.LENGTH_LONG).show()
+                    } else {
+                        etxtCodeViewModel.reverseShowDialogState()
+                    }
+                },
+                shape = RoundedCornerShape(50),
+            ) {
+                Text(text = "Login")
+                Icon(imageVector = Icons.TwoTone.Login,
+                    contentDescription = null,
+                    Modifier.padding(start = 10.dp))
+            }
+        }
+    }
+}
+
+// EtxtCode State Hosting
+class EtxtCodeViewModel : ViewModel() {
+    private val _showDialog: MutableLiveData<Boolean> = MutableLiveData<Boolean>(false)
+    private val _imageBitmap = MutableLiveData<ImageBitmap>()
+    private val _etxtcode: MutableLiveData<String> = MutableLiveData("")
+
+    val showDialog: LiveData<Boolean> = _showDialog
+    val etxtImageBitmap: LiveData<ImageBitmap> = _imageBitmap
+    val etxtCode: LiveData<String> = _etxtcode
+
+    init {
+        requestEtxtImageBitmap()
+    }
+
+    private fun requestEtxtImageBitmap() {
+        viewModelScope.launch {
+            onEtxtImageBitmapChange(user.getWebapEtxtBitmap())
+        }
+    }
+
+    private fun onEtxtImageBitmapChange(newEtxtImageBitmap: ImageBitmap) {
+        _imageBitmap.value = newEtxtImageBitmap
+    }
+
+    fun onEtxtCodeChange(newEtxtCode: String) {
+        _etxtcode.value = newEtxtCode
+    }
+
+    fun reverseShowDialogState() {
+        _showDialog.value = (_showDialog.value)!!.not()
+    }
+}
+
+@Composable
+fun ShowDialogBase(
+    context: Context = LocalContext.current,
+    etxtCodeViewModel: EtxtCodeViewModel = viewModel(),
+) {
+
+    val etxtCode: String by etxtCodeViewModel.etxtCode.observeAsState("")
+    val etxtImageBitmap: ImageBitmap by etxtCodeViewModel.etxtImageBitmap
+        .observeAsState(ImageBitmap(width = 85, height = 40))
+    val showDialogState: Boolean by etxtCodeViewModel.showDialog.observeAsState(false)
+
+    fun onPositiveCallback() {
+        etxtCodeViewModel.reverseShowDialogState()
+    }
+
+    fun onNegativeCallback() {
+        etxtCodeViewModel.reverseShowDialogState()
+        Toast.makeText(context, "Enter validate code before login!", Toast.LENGTH_LONG)
+            .show()
+    }
+    if (showDialogState)
+        AlertDialogForEtxtCode(
+            etxtCode = etxtCode,
+            etxtImageBitmap = etxtImageBitmap,
+            onEtxtCodeChange = { etxtCodeViewModel.onEtxtCodeChange(it) },
+            onPositiveClick = { onPositiveCallback() },
+            onNegativeClick = { onNegativeCallback() })
+
+}
+
+
+@OptIn(ExperimentalMaterial3Api::class)
+@Composable
+@SuppressLint("CoroutineCreationDuringComposition")
+fun AlertDialogForEtxtCode(
+    etxtCode: String,
+    etxtImageBitmap: ImageBitmap,
+    onEtxtCodeChange: (String) -> Unit,
+    onPositiveClick: () -> Unit,
+    onNegativeClick: () -> Unit,
+) {
+    // https://stackoverflow.com/questions/68639232/jetpack-compose-how-to-create-an-imagebitmap-with-specific-size-and-configuratio
+    Dialog(onDismissRequest = {}) {
+        OutlinedCard(
+            elevation = CardDefaults.outlinedCardElevation(15.dp),
+            modifier = Modifier.aspectRatio(1F))
+        {
+            // 應該要交給observer處理
+            if (etxtImageBitmap != ImageBitmap(width = 85, height = 40)) {
+                Column(modifier = Modifier.padding(8.dp)) {
+                    Text(text = "Please input validate code below:", fontSize = 20.sp)
+                    Spacer(modifier = Modifier.padding(10.dp))
+
+                    Image(
+                        bitmap = etxtImageBitmap,
+                        contentDescription = null,
+                        alignment = Alignment.Center,
+                        modifier = Modifier.aspectRatio(5F),
+                    )
+                    Spacer(modifier = Modifier.padding(5.dp))
+                    OutlinedTextField(
+
+                        value = etxtCode,
+                        onValueChange = { onEtxtCodeChange(it) },
+                        label = { Text(text = "Validate Code") },
+                        singleLine = true,
+                        leadingIcon = {
+                            Icon(imageVector = Icons.TwoTone.HowToReg,
+                                "")
+                        },
+                        shape = RoundedCornerShape(50),
+                        modifier = Modifier
+                            .focusTarget()
+                            .fillMaxWidth(),
+                        keyboardOptions = KeyboardOptions(imeAction = ImeAction.Done),
+
+                        )
+                    Spacer(modifier = Modifier.padding(5.dp))
+                    //button
+                    Row(
+                        modifier = Modifier.fillMaxWidth()
+                    ) {
+                        OutlinedButton(onClick = {
+                            onNegativeClick.invoke()
+                        }) {
+                            Icon(imageVector = Icons.TwoTone.Cancel,
+                                modifier = Modifier.padding(end = 4.dp),
+                                contentDescription = null)
+                            Text(text = "Cancel login")
+                        }
+                        Spacer(modifier = Modifier.width(4.dp))
+                        OutlinedButton(
+                            onClick = onPositiveClick,
+                            modifier = Modifier.padding(end = 4.dp)) {
+                            Icon(imageVector = Icons.TwoTone.Verified,
+                                contentDescription = null,
+                                modifier = Modifier.padding(end = 4.dp))
+                            Text(text = "Login")
+                        }
+                    }
+                }
+            } else {
+                Column() {
+                    Text(text = "Please wait...", fontSize = 20.sp)
+                    Box(modifier = Modifier.fillMaxSize(),
+                        contentAlignment = Alignment.Center) {
+                        CircularProgressIndicator()
+                    }
+                }
+
+            }
+        }
+    }
+}
