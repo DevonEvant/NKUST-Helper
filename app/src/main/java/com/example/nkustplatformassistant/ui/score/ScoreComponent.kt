8f1d4471059dd15b56a89441fd82805389f0205e
DevonEvant <horningch@gmail.com>
2022-08-31 00:52:37 +0800

    disign Score Screen(not complete)

---
+++ app/src/main/java/com/example/nkustplatformassistant/ui/score/ScoreComponent.kt
@@ -0,0 +1,29 @@
+package com.example.nkustplatformassistant.ui.score
+
+import androidx.compose.foundation.background
+import androidx.compose.foundation.horizontalScroll
+import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.lazy.grid.GridCells
+import androidx.compose.foundation.lazy.grid.GridItemSpan
+import androidx.compose.foundation.lazy.grid.LazyGridScope
+import androidx.compose.foundation.lazy.grid.LazyVerticalGrid
+import androidx.compose.foundation.rememberScrollState
+import androidx.compose.foundation.shape.CutCornerShape
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.AutoAwesome
+import androidx.compose.material3.*
+import androidx.compose.material3.AlertDialogDefaults.shape
+import androidx.compose.runtime.*
+import androidx.compose.runtime.livedata.observeAsState
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.graphics.Brush
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.Shape
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.lifecycle.LiveData
+import com.example.nkustplatformassistant.ui.theme.Nkust_platform_assistantTheme
+import com.soywiz.klogger.AnsiEscape
+
