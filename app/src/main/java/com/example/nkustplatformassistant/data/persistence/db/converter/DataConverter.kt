4ab1f57069e33cafb5c0b5ea91d28e3b685523a2
lrnbrad <lrnbrad@gmail.com>
2022-09-01 03:19:18 +0800

    There is no usage here

--- app/src/main/java/com/example/nkustplatformassistant/data/persistence/db/converter/DataConverter.kt
+++ app/src/main/java/com/example/nkustplatformassistant/data/persistence/db/converter/DataConverter.kt
@@ -1,26 +1,5 @@
 package com.example.nkustplatformassistant.data.persistence.db.converter
 
-import androidx.room.TypeConverter
-import com.example.nkustplatformassistant.data.remote.Score
-
 class DataConverter {
-    @TypeConverter
-    fun fromSubjectToDB(score: Score): List<String> {
-        return listOf(
-            score.subjectName,
-            score.midScore,
-            score.finalScore
-        )
-    }
-
-    @TypeConverter
-    fun fromDBToSubject(listSubject: List<String>): Score {
-        return Score(
-            listSubject[0],
-            listSubject[1],
-            listSubject[2]
-        )
-    }
-
 
 }
