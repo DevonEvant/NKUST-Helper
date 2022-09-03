8496a514c4cd888e97c7a7a565fef4b7c7ce4b96
lrnbrad <lrnbrad@gmail.com>
2022-09-04 03:45:35 +0800

    Modify CourseEntity to fit logic.

--- app/src/main/java/com/example/nkustplatformassistant/data/persistence/db/entity/CourseEntity.kt
+++ app/src/main/java/com/example/nkustplatformassistant/data/persistence/db/entity/CourseEntity.kt
@@ -7,16 +7,14 @@ import androidx.room.PrimaryKey
 
 @Entity(tableName = "course_table")
 data class Course(
-    @NonNull @ColumnInfo val courseId: Int,
+    @NonNull @PrimaryKey @ColumnInfo val courseId: Int,
     @ColumnInfo val professor: String,
     @ColumnInfo val courseName: String,
     @ColumnInfo val className: String,
     @ColumnInfo val classLocation: String,
+    @ColumnInfo val classTime: String,
     @ColumnInfo val classGroup: String,
     @ColumnInfo val credits: String,
     @ColumnInfo val teachingHours: String,
     @ColumnInfo val importance: Boolean,
-    @ColumnInfo val weekday: String,
-    @ColumnInfo val startTime: String,
-    @ColumnInfo val endTime: String,
 )
