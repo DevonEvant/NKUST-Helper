7c9619517b3fec79acb55aac9171ee69b65373c2
DevonEvant <horningch@gmail.com>
2022-09-04 13:47:35 +0800

    增加註解並修改部分函數

--- app/src/main/java/com/example/nkustplatformassistant/data/structure.kt
+++ app/src/main/java/com/example/nkustplatformassistant/data/structure.kt
@@ -2,20 +2,26 @@ package com.example.nkustplatformassistant.data
 
 import java.lang.Exception
 
-enum class Weeks(val shortCode: String, val cn: String, val shortCnCode: Char) {
-    Mon("M", "星期一", '一'),
-    Tue("T", "星期二", '二'),
-    Wed("W", "星期三", '三'),
-    Thu("T", "星期四", '四'),
-    Fri("F", "星期五", '五'),
-    Sat("S", "星期六", '六'),
-    Sun("S", "星期日", '日');
+enum class Weeks(val shortCode: Char, val cn: String, val shortCnCode: Char) {
+    Mon('M', "星期一", '一'),
+    Tue('T', "星期二", '二'),
+    Wed('W', "星期三", '三'),
+    Thu('T', "星期四", '四'),
+    Fri('F', "星期五", '五'),
+    Sat('S', "星期六", '六'),
+    Sun('S', "星期日", '日');
 
+    /**
+     * you can use operator '[ ]' to get the corresponding item from ordinal
+     */
     operator fun get(ordinal: Int): Weeks {
         return values()[ordinal]
     }
 
-    fun findByShortCnCode(shortCnCode: Char): Weeks? {
+    /**
+     * get the corresponding item by weekShortCnCode(like '一','四','五','日')
+     */
+    fun getByShortCnCode(shortCnCode: Char): Weeks? {
         for (v in values()) {
             if (v.shortCnCode == shortCnCode)
                 return v
@@ -24,36 +30,54 @@ enum class Weeks(val shortCode: String, val cn: String, val shortCnCode: Char) {
     }
 }
 
-enum class CurriculumTime(val id: String, val startTime: String, val endTime: String) {
-    _M("M", "07:10", "08:00"),
-    _1("1", "08:10", "09:00"),
-    _2("2", "09:10", "10:00"),
-    _3("3", "10:10", "11:00"),
-    _4("4", "11:10", "12:00"),
-    _A("A", "12:30", "13:20"),
-    _5("5", "13:30", "14:20"),
-    _6("6", "14:30", "15:20"),
-    _7("7", "15:30", "16:20"),
-    _8("8", "16:30", "17:20"),
-    _9("9", "17:30", "18:20");
 
+enum class CurriculumTime(val id: Char, val startTime: String, val endTime: String) {
+    _M('M', "07:10", "08:00"),
+    _1('1', "08:10", "09:00"),
+    _2('2', "09:10", "10:00"),
+    _3('3', "10:10", "11:00"),
+    _4('4', "11:10", "12:00"),
+    _A('A', "12:30", "13:20"),
+    _5('5', "13:30", "14:20"),
+    _6('6', "14:30", "15:20"),
+    _7('7', "15:30", "16:20"),
+    _8('8', "16:30", "17:20"),
+    _9('9', "17:30", "18:20");
 
+    /**
+     * you can use operator '[ ]' to get the corresponding item from ordinal
+     */
     operator fun get(ordinal: Int): CurriculumTime {
         return values()[ordinal]
     }
 
+    /**
+     * you can use operator "rangeTo" (like 1..3) and return ClosedRange<CurriculumTime>
+     */
     operator fun rangeTo(end: CurriculumTime): ClosedRange<CurriculumTime> {
+        if (this@CurriculumTime.ordinal > end.ordinal)
+            throw Error("start cannot bigger then end")
+
         return object : ClosedRange<CurriculumTime> {
             override val start: CurriculumTime = this@CurriculumTime
             override val endInclusive: CurriculumTime = end
+
+            /**
+             * you can use operator "in" in object "ClosedRange<CurriculumTime>"
+             * (like CurriculumTime._A in CurriculumTime._3..CurriculumTime._7 == ture)
+             */
+            override operator fun contains(value: CurriculumTime): Boolean =
+                (endInclusive.ordinal >= value.ordinal && value.ordinal >= start.ordinal)
+
+            override fun isEmpty(): Boolean = start.ordinal > endInclusive.ordinal
+
         }
     }
 
-    operator fun ClosedRange<CurriculumTime>.contains(at: CurriculumTime): Boolean {
-        return endInclusive.ordinal >= at.ordinal && at.ordinal >= start.ordinal
-    }
-
-    fun findById(id: String): CurriculumTime? {
+    /**
+     * get the corresponding item by CurriculumTimeId(like 'M','1','A','9')
+     */
+    fun getById(id: Char): CurriculumTime? {
         for (v in values()) {
             if (v.id == id)
                 return v
