4954e7d2b6d961889b42179b6c3371c01b7602c5
lrnbrad <lrnbrad@gmail.com>
2022-09-05 03:43:56 +0800

    Add TypeConverter to convert time data to enum class defined in structure.kt

--- app/src/main/java/com/example/nkustplatformassistant/data/persistence/db/dao/CourseDao.kt
+++ app/src/main/java/com/example/nkustplatformassistant/data/persistence/db/dao/CourseDao.kt
@@ -1,19 +1,18 @@
 package com.example.nkustplatformassistant.data.persistence.db.dao
 
 import androidx.room.*
-import com.example.nkustplatformassistant.data.persistence.db.entity.Calender
-import com.example.nkustplatformassistant.data.persistence.db.entity.Course
-import kotlinx.coroutines.flow.Flow
+import com.example.nkustplatformassistant.data.persistence.db.converter.DataConverter
+import com.example.nkustplatformassistant.data.persistence.db.entity.CourseEntity
 
 @Dao
 interface CourseDao {
 
-    @Query("SELECT professor ,courseName, className," +
-            " classLocation, weekday, startTime, endTime FROM COURSE_TABLE")
-    suspend fun getBasicClassData(courseId: Int)
+    @Query("SELECT * FROM COURSE_TABLE WHERE courseId = :courseId")
+    @TypeConverters(DataConverter::class)
+    suspend fun getSpecCourseById(courseId: Int): CourseEntity
 
     @Insert(onConflict = OnConflictStrategy.REPLACE)
-    suspend fun insertCourse(vararg course: Course)
+    suspend fun insertCourse(course: CourseEntity)
 
     @Query("DELETE FROM COURSE_TABLE WHERE courseId = :courseId")
     suspend fun deleteSpecificCourse(courseId: Int)
