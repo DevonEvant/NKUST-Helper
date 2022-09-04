e5084b112a787aed5a4b888f5969d4881e97053f
lrnbrad <lrnbrad@gmail.com>
2022-09-05 03:56:38 +0800

    Change from data class Course in structure.kt to CourseEntity in CourseEntity.kt

--- app/src/main/java/com/example/nkustplatformassistant/ui/curriculum/CurriculumViewModel.kt
+++ app/src/main/java/com/example/nkustplatformassistant/ui/curriculum/CurriculumViewModel.kt
@@ -3,18 +3,18 @@ package com.example.nkustplatformassistant.ui.curriculum
 import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
-import com.example.nkustplatformassistant.data.persistence.db.entity.Course
+import com.example.nkustplatformassistant.data.persistence.db.entity.CourseEntity
 
 
 class CurriculumViewModel : ViewModel() {
     private val _timeVisibility = MutableLiveData(false)
     private val _timeCodeVisibility = MutableLiveData(false)
-    private val _courses = MutableLiveData(mutableListOf<Course>())
+    private val _courses = MutableLiveData(mutableListOf<CourseEntity>())
 
 
     val timeVisibility: LiveData<Boolean> = _timeVisibility
     val timeCodeVisibility: LiveData<Boolean> = _timeCodeVisibility
-    val courses: LiveData<MutableList<Course>> = _courses
+    val courses: LiveData<MutableList<CourseEntity>> = _courses
 
     init {
         if (_courses.value!!.isEmpty())
