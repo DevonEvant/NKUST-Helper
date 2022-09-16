31229449065b9bfcc1b95d8489eb687fe09b5c6a
lrnbrad <lrnbrad@gmail.com>
2022-09-16 21:15:12 +0800

    Fix app crash with liveData's usage before emit

--- app/src/main/java/com/example/nkustplatformassistant/ui/home/HomeViewModel.kt
+++ app/src/main/java/com/example/nkustplatformassistant/ui/home/HomeViewModel.kt
@@ -1,24 +1,39 @@
 package com.example.nkustplatformassistant.ui.home
 
 import androidx.lifecycle.*
+import com.example.nkustplatformassistant.data.CourseTime
+import com.example.nkustplatformassistant.data.CurriculumTime
+import com.example.nkustplatformassistant.data.ResultOf
 import kotlinx.coroutines.launch
 import com.example.nkustplatformassistant.data.persistence.DataRepository
+import com.example.nkustplatformassistant.data.persistence.db.entity.Calender
 import com.example.nkustplatformassistant.data.persistence.db.entity.CourseEntity
 import com.soywiz.korma.geom.bezier.SegmentEmitter.emit
 import kotlinx.coroutines.Dispatchers
+import java.time.DayOfWeek
+import java.time.LocalDate
+import java.time.format.DateTimeFormatter
+import java.util.*
+import java.util.concurrent.Flow
 
 class HomeViewModel(private val dataRepository: DataRepository) : ViewModel() {
 
 
     // https://stackoverflow.com/questions/71709590/how-to-initialize-a-field-in-viewmodel-with-suspend-method
 
+    val dbHasData: LiveData<Boolean> = liveData {
+        emit(dataRepository.checkDataIsReady())
+    }
+
     fun startFetch(force: Boolean): LiveData<Float> = liveData {
-        dataRepository.fetchAllScoreToDB()
-        emit(0.33F)
-        dataRepository.fetchCourseDataToDB()
-        emit(0.67F)
-        // TODO: fetch Schedule to DB, progress didn't change
-        emit(1F)
+        if (force) {
+            dataRepository.fetchAllScoreToDB()
+            emit(0.33F)
+            dataRepository.fetchCourseDataToDB()
+            emit(0.67F)
+            // TODO: fetch Schedule to DB, progress didn't change
+            emit(1F)
+        }
     }
 
     fun clearDB(force: Boolean) {
@@ -29,34 +44,17 @@ class HomeViewModel(private val dataRepository: DataRepository) : ViewModel() {
         }
     }
 
-    val dbHasData: LiveData<Boolean> = liveData {
-        emit(dataRepository.checkDataIsReady())
-    }
-
     enum class SubjectWidgetEnum {
         CourseName, ClassName, ClassLocation, ClassTime, ClassGroup, Professor, StartTime, EndTime,
     }
 
-    val courseWidgetParams: LiveData<Map<SubjectWidgetEnum, String>> = liveData {
+    // ResultOf.Error 表示今天沒課
+    val courseWidgetParams: LiveData<ResultOf<*>> = liveData {
         lateinit var courseEntity: CourseEntity
         viewModelScope.launch(Dispatchers.IO) {
-            courseEntity = dataRepository.getCurrentCourse()
-
             emit(
-                mapOf(
-                    SubjectWidgetEnum.CourseName to courseEntity.courseName,
-                    SubjectWidgetEnum.ClassName to courseEntity.className,
-                    SubjectWidgetEnum.ClassLocation to courseEntity.classLocation,
-                    SubjectWidgetEnum.ClassTime to courseEntity.classTime,
-                    SubjectWidgetEnum.ClassGroup to courseEntity.classGroup,
-                    SubjectWidgetEnum.Professor to courseEntity.professor,
-                    //                SubjectWidgetEnum.StartTime to courseEntity.courseTime[0].curriculumTimeRange.start.startTime,
-                    SubjectWidgetEnum.StartTime to courseEntity.courseTime[0].curriculumTimeRange.start.time.start.toIsoDescription(),
-//                SubjectWidgetEnum.EndTime to courseEntity.courseTime[1].curriculumTimeRange.endInclusive.endTime
-                    SubjectWidgetEnum.EndTime to courseEntity.courseTime[1].curriculumTimeRange.endInclusive.time.endInclusive.toIsoDescription()
-                )
+                dataRepository.getCurrentCourse()
             )
         }
-
     }
 }
