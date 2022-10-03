d5259653dad2c47b6530996e53d0b3963e5df483
lrnbrad <lrnbrad@gmail.com>
2022-10-03 19:52:01 +0800

    currently not use

--- app/src/test/java/com/example/nkustplatformassistant/repositoryTest.kt
+++ app/src/test/java/com/example/nkustplatformassistant/repositoryTest.kt
@@ -4,28 +4,28 @@ import com.example.nkustplatformassistant.data.remote.NkustAccessor
 import kotlinx.coroutines.runBlocking
 
 
-fun main() {
-    System.setProperty("com.sun.security.enableAIAcaIssuers", "true");
-
-    val accessor = NkustAccessor()
-
-    runBlocking {
-        accessor.getAndSaveWebapEtxtImage()
-        println("Input Etxt:")
-        val etxt = readLine()
-
-        println("Login State: " +
-                accessor.loginWebap("C110152351","c110ankust",etxt!!)
-        )
-
-//        accessor.getCurrYearAndSemester().split(",").let {
-//            accessor.getSpecCurriculum(it[0],it[1])
-//        }
-
-            accessor.getSpecCurriculum("110","1").let {
-                println(it.toString())
-            }
-
-
-    }
-}
+//fun main() {
+//    System.setProperty("com.sun.security.enableAIAcaIssuers", "true");
+//
+//    val accessor = NkustAccessor()
+//
+//    runBlocking {
+//        accessor.getAndSaveWebapEtxtImage()
+//        println("Input Etxt:")
+//        val etxt = readLine()
+//
+//        println("Login State: " +
+//                accessor.loginWebap("C110152351","c110ankust",etxt!!)
+//        )
+//
+////        accessor.getCurrYearAndSemester().split(",").let {
+////            accessor.getSpecCurriculum(it[0],it[1])
+////        }
+//
+//            accessor.getSpecCurriculum("110","1").let {
+//                println(it.toString())
+//            }
+//
+//
+//    }
+//}
