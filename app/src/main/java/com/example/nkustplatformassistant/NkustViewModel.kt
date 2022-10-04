d2937d9507481820e587a1a40e630adde9f403b3
lrnbrad <lrnbrad@gmail.com>
2022-10-05 01:10:34 +0800

    remove unused comments

--- app/src/main/java/com/example/nkustplatformassistant/NkustViewModel.kt
+++ app/src/main/java/com/example/nkustplatformassistant/NkustViewModel.kt
@@ -5,13 +5,6 @@ import com.example.nkustplatformassistant.data.persistence.DataRepository
 import kotlinx.coroutines.launch
 
 class NkustViewModel(private val dataRepository: DataRepository) : ViewModel() {
-
-//    init {
-//        viewModelScope.launch {
-//
-//        }
-//    }
-
     fun dataAvailability() = liveData {
             emit (dataRepository.checkDataIsReady())
     }
