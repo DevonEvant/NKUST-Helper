bcb37c09cf76dc62737be982b2a46ccd279ade22
lrnbrad <lrnbrad@gmail.com>
2022-10-14 05:03:28 +0800

    Move ScoreOther to ScoreOtherWidget for later usage in homeComponent

--- app/src/main/java/com/example/nkustplatformassistant/ui/score/ScoreComponent.kt
+++ app/src/main/java/com/example/nkustplatformassistant/ui/score/ScoreComponent.kt
@@ -1,5 +1,6 @@
 package com.example.nkustplatformassistant.ui.score
 
+import android.content.res.Resources
 import androidx.compose.foundation.*
 import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.lazy.LazyColumn
@@ -27,6 +28,7 @@ import com.example.nkustplatformassistant.data.persistence.db.entity.ScoreOtherE
 import com.example.nkustplatformassistant.navigation.Screen
 import com.example.nkustplatformassistant.ui.curriculum.SemesterSelector
 import com.example.nkustplatformassistant.ui.theme.Nkust_platform_assistantTheme
+import java.security.AccessController.getContext
 
 @Composable
 fun ScoreContent(scoreViewModel: ScoreViewModel, navController: NavController) {
@@ -34,7 +36,7 @@ fun ScoreContent(scoreViewModel: ScoreViewModel, navController: NavController) {
     val scores by scoreViewModel.scores.observeAsState()
     val scoreOther by scoreViewModel.scoreOther.observeAsState(
         ScoreOtherEntity(
-            "-1", "-1", "-1", "0", "0",
+            "-1", "-1", "-1", "", "",
             null, null, null, null,
         )
     )
@@ -70,73 +72,77 @@ fun ScoreContent(scoreViewModel: ScoreViewModel, navController: NavController) {
                 }
             }
 
-
-            val classRanking = if (scoreOther.classRanking == null) 0 else scoreOther.classRanking!!
-            val deptRanking = if (scoreOther.deptRanking == null) 0 else scoreOther.deptRanking!!
-
-            val displayList: List<Pair<Int, Any>> = listOf(
-                R.string.score_beheavior to scoreOther.behaviorScore,
-                R.string.score_average to scoreOther.average,
-                R.string.score_class_ranking to classRanking,
-                R.string.score_dept_ranking to deptRanking
-            )
-
             item {
-                Row {
-                    Column(
-                        modifier = Modifier
-                            .weight(0.5F)
-                            .padding(start = 8.dp)
+                ScoreOtherWidget(scoreOther)
+            }
+        }
+    }
+}
 
-                    ) {
-                        for (item in displayList) {
-                            Text(
-                                stringResource(item.first),
-                                fontWeight = FontWeight.Bold,
-                                textAlign = TextAlign.Center,
-                            )
-                            Text(
-                                item.second.toString(),
-                                textAlign = TextAlign.Center,
-                            )
-                        }
-                    }
+@Composable
+fun ScoreOtherWidget(scoreOther: ScoreOtherEntity) {
+    val localContext = LocalContext.current
+    val displayList: List<Pair<Int, Any>> = listOf(
+        R.string.score_beheavior to scoreOther.behaviorScore.ifEmpty { localContext.getString(R.string.nullstring) },
+        R.string.score_average to scoreOther.average.ifEmpty { localContext.getString(R.string.nullstring) },
+        R.string.score_class_ranking to
+                if (scoreOther.classRanking.toString() == "null") localContext.getString(R.string.nullstring)
+                else scoreOther.classRanking.toString(),
+        R.string.score_dept_ranking to
+                if (scoreOther.deptRanking.toString() == "null") localContext.getString(R.string.nullstring)
+                else scoreOther.deptRanking.toString()
+    )
 
-                    @Composable
-                    fun GradeBar(percent: Float) {
-                        Canvas(modifier = Modifier
-                            .height(5.dp)
-                            .fillMaxWidth(0.7F)) {
-                            drawRoundRect(color = Color.Red)
-                            drawRoundRect(color = Color.Green,
-                                size = Size(width = size.width * (1 - percent), height = size.height))
+    Row {
+        Column(
+            modifier = Modifier
+                .weight(0.5F)
+                .fillMaxSize()
+                .padding(start = 8.dp),
+            horizontalAlignment = Alignment.Start
 
-                        }
-                    }
+        ) {
+            for (item in displayList) {
+                Text(
+                    stringResource(item.first),
+                    fontWeight = FontWeight.Bold,
+                )
+                Text(item.second.toString())
+            }
+        }
 
+        @Composable
+        fun GradeBar(percent: Float) {
+            Canvas(modifier = Modifier
+                .height(5.dp)
+                .fillMaxWidth(0.7F)) {
+                drawRoundRect(color = Color.Red)
+                drawRoundRect(color = Color.Green,
+                    size = Size(width = size.width * (1 - percent),
+                        height = size.height))
 
-                    Column(
-                        modifier = Modifier.weight(0.5F),
-                        verticalArrangement = Arrangement.spacedBy(20.dp),
-                        horizontalAlignment = Alignment.CenterHorizontally
-                    ) {
-                        if (scoreOther.deptRanking != null && scoreOther.deptPeople != null &&
-                            scoreOther.classRanking != null && scoreOther.classPeople != null
-                        ) {
-                            val classPercent: Float =
-                                scoreOther.classRanking!!.toFloat() / scoreOther.classPeople!!
-                            val deptPercent: Float =
-                                scoreOther.deptRanking!!.toFloat() / scoreOther.deptPeople!!
+            }
+        }
 
-                            Text(stringResource(R.string.score_class_ranking_rate,
-                                classPercent * 100), fontWeight = FontWeight.Bold)
-                            GradeBar(percent = classPercent)
-                            Text(stringResource(R.string.score_dept_ranking_rate,
-                                deptPercent * 100), fontWeight = FontWeight.Bold)
-                            GradeBar(percent = deptPercent)
-                        }
-                    }
-                }
+        Column(
+            modifier = Modifier.weight(0.5F),
+            verticalArrangement = Arrangement.spacedBy(20.dp),
+            horizontalAlignment = Alignment.CenterHorizontally
+        ) {
+            if (scoreOther.deptRanking != null && scoreOther.deptPeople != null &&
+                scoreOther.classRanking != null && scoreOther.classPeople != null
+            ) {
+                val classPercent: Float =
+                    scoreOther.classRanking.toFloat() / scoreOther.classPeople
+                val deptPercent: Float =
+                    scoreOther.deptRanking.toFloat() / scoreOther.deptPeople
+
+                Text(stringResource(R.string.score_class_ranking_rate,
+                    classPercent * 100), fontWeight = FontWeight.Bold)
+                GradeBar(percent = classPercent)
+                Text(stringResource(R.string.score_dept_ranking_rate,
+                    deptPercent * 100), fontWeight = FontWeight.Bold)
+                GradeBar(percent = deptPercent)
             }
         }
     }
@@ -175,7 +181,6 @@ fun ScoresDataTable(scores: List<ScoreEntity?>) {
             )
         }
 
-
         tableLine()
 
         scores.forEachIndexed { index, score ->
@@ -204,11 +209,9 @@ fun ScoresDataTable(scores: List<ScoreEntity?>) {
                 }
             }
 
-
             if (index < scores.lastIndex)
                 tableLine()
         }
-
     }
 }
 
