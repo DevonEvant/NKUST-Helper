0c79a96497136c1fba4d03b633034c8faef64892
lrnbrad <lrnbrad@gmail.com>
2022-10-19 06:21:56 +0800

    update ViewCompat.getWindowInsetsController with WindowCompat.getInsetsController

--- app/src/main/java/com/example/nkustplatformassistant/ui/theme/Theme.kt
+++ app/src/main/java/com/example/nkustplatformassistant/ui/theme/Theme.kt
@@ -3,17 +3,13 @@ package com.example.nkustplatformassistant.ui.theme
 import android.app.Activity
 import android.os.Build
 import androidx.compose.foundation.isSystemInDarkTheme
-import androidx.compose.material3.MaterialTheme
-import androidx.compose.material3.darkColorScheme
-import androidx.compose.material3.dynamicDarkColorScheme
-import androidx.compose.material3.dynamicLightColorScheme
-import androidx.compose.material3.lightColorScheme
+import androidx.compose.material3.*
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.SideEffect
 import androidx.compose.ui.graphics.toArgb
 import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.platform.LocalView
-import androidx.core.view.ViewCompat
+import androidx.core.view.WindowCompat
 
 private val DarkColorScheme = darkColorScheme(
     primary = Purple80,
@@ -43,7 +39,7 @@ fun Nkust_platform_assistantTheme(
     darkTheme: Boolean = isSystemInDarkTheme(),
     // Dynamic color is available on Android 12+
     dynamicColor: Boolean = true,
-    content: @Composable () -> Unit
+    content: @Composable () -> Unit,
 ) {
     val colorScheme = when {
         dynamicColor && Build.VERSION.SDK_INT >= Build.VERSION_CODES.S -> {
@@ -54,17 +50,25 @@ fun Nkust_platform_assistantTheme(
         else -> LightColorScheme
     }
     val view = LocalView.current
+    val activity = LocalContext.current as Activity
     if (!view.isInEditMode) {
         SideEffect {
             (view.context as Activity).window.statusBarColor = colorScheme.primary.toArgb()
-            ViewCompat.getWindowInsetsController(view)?.isAppearanceLightStatusBars = darkTheme
+//            ViewCompat.getWindowInsetsController(view)?.isAppearanceLightStatusBars = darkTheme
+            WindowCompat.getInsetsController(activity.window, view).isAppearanceLightStatusBars =
+                darkTheme
         }
     }
 
     MaterialTheme(
         colorScheme = colorScheme,
         typography = Typography,
-        content = content
+        content = {
+            Surface(color = MaterialTheme.colorScheme.surface) {
+                content.invoke()
+            }
+        }
+
     )
 }
 
